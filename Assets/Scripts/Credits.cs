a32f5c2f6f603c1abad3f659b4fde7a13dea097d
tomdotscott <tom.scott45456@gmail.com>
2020-04-29 20:22:04 +0100

    Credits
    A credits scene was added at the end of the game

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI completedText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 0;
+        if(PlayerPrefs.GetString("MadHatter") == "True")
+        {
+            count++;
+        }
+        if(PlayerPrefs.GetString("Teapot") == "True")
+        {
+            count++;
+        }
+        if(PlayerPrefs.GetString("PlayingCard") == "True")
+        {
+            count++;
+        }
+        if(PlayerPrefs.GetString("CheshireCat") == "True")
+        {
+            count++;
+        }
+        completedText.text = "In your journey to have tea with the Queen of Hearts, you found " + count + " of 5 secrets.";
+    }
+}
