1708fcfb1ad336d7b51b3281264fae7064e775e9
tomdotscott <tom.scott45456@gmail.com>
2020-05-04 20:04:52 +0100

    Loading Screen
    The game now has a loading screen to help better transition between scenes

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Slider progressSlider;
+
+    public void LoadLevel()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(loadAsynchronously(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    private IEnumerator loadAsynchronously(int buildIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(buildIndex);
+        while (!operation.isDone)
+        {
+            progressSlider.value = Mathf.Clamp01(operation.progress / .9f);
+            yield return null; 
+        }
+    }
+}
